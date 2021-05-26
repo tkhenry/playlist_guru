1662f192733eb387a759c6b0973f6bcce83cc087
Tom Henry <tommyhenry35@gmail.com>
2021-05-26 17:36:10 -0500

    Tokens all gathered

--- src/authenticate.py
+++ src/authenticate.py
@@ -2,10 +2,10 @@ import requests
 import urllib
 import base64
 
-
 auth_url = "https://accounts.spotify.com/authorize"
-client_id = '9427317fbcc04f0d966ebb2255270347'
-client_secret = '293eeb24c82a4e1f867adab6453e6e96'
+client_id = ''
+client_secret = ''
+uri_red = 'http://127.0.0.1:5000/auth'
 redirect_uri = {'redirect_uri':'http://127.0.0.1:5000/auth'}
 token_url = 'https://accounts.spotify.com/api/token'
 scope = 'playlist-read-private'
@@ -15,28 +15,23 @@ client_credentials = f'{client_id}:{client_secret}'
 client_credentials = base64.b64encode(client_credentials.encode())
 
 
-#get the request
+#get the user access code
+def request_string():
+    request_string = f'{auth_url}?client_id={client_id}&response_type={response_type}&scope={scope}&{urllib.parse.urlencode(redirect_uri)}'
+    return request_string
 
-request_string = f'{auth_url}?client_id={client_id}&response_type={response_type}&scope={scope}&{urllib.parse.urlencode(redirect_uri)}'
-print(request_string)
-#get_response = requests.get(request_string)
-
-
-
-#Other token request data
-token_data = {
-    "grant_type" : "client_credentials"
-}
-token_headers = {
-    "Authorization" : f"Basic {client_credentials.decode()}"
-}
-
-token_response = requests.post(token_url, data=token_data, headers = token_headers)
-token_json = token_response.json()
-access_token = token_json['access_token']
-token_type = token_json['token_type']
-token_expires = token_json['expires_in']
-print("Token:",access_token, "Type:", token_type, "Expires In:", token_expires)
+#get all the other access codes
+def get_tokens(code):
+    token_data = {
+        "grant_type": "authorization_code",
+        "code": f"{code}",
+        "redirect_uri": f"{uri_red}",
+    }
+    token_headers = {
+        "Authorization": f"Basic {client_credentials.decode()}"
+    }
+    token_response = requests.post(token_url, data=token_data, headers=token_headers)
+    return token_response.json()
 
 
 
--- src/main.py
+++ src/main.py
@@ -5,23 +5,24 @@ app = Flask(__name__)
 
 '''GLOBALS'''
 user_code = ''
-
-
+token_json = {}
 
 @app.route('/auth')
 def codes():
     global user_code
+    global token_json
+    #Gets the user authentication code from the url hash
     user_code = request.args.get('code', default='', type = str)
+    token_json = authenticate.get_tokens(user_code)
+    print(token_json)
     return redirect(url_for('home'))
-
 
 @app.route('/')
 def something():
-    return "Hello World"
+    return redirect(authenticate.request_string())
 
 @app.route('/home')
 def home():
-    print(user_code)
     return "You are home"
 
 
