20748eb2696b76b0d34ca73cdfa58396213a33be
Tom Henry <69489314+tkhenry@users.noreply.github.com>
2021-05-27 17:42:56 -0500

    Update authenticate.py
    Security

--- src/authenticate.py
+++ src/authenticate.py
@@ -3,8 +3,8 @@ import urllib
 import base64
 
 auth_url = "https://accounts.spotify.com/authorize"
-client_id = '9427317fbcc04f0d966ebb2255270347'
-client_secret = '293eeb24c82a4e1f867adab6453e6e96'
+client_id = ''
+client_secret = ''
 uri_red = 'http://127.0.0.1:5000/auth'
 redirect_uri = {'redirect_uri':'http://127.0.0.1:5000/auth'}
 token_url = 'https://accounts.spotify.com/api/token'
